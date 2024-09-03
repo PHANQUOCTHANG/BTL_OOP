ac86042ec3c900a7d30fdbaf2331f559f5a8746d
phu188 <tranvanphu1882005z@gmail.com>
2024-09-03 15:18:08 +0700

    add

--- BTL_OOP/main.cpp
+++ BTL_OOP/main.cpp
@@ -2,15 +2,5 @@
 #include "function.cpp"
 #include "username.cpp"
 int main() {
-<<<<<<< HEAD
     userProcess();
-    
-
-=======
-    cout << "Hello World" ;
-    int a = 1 ;
-    int b=2;
-    int c = 3 ;
-    int d = 10 ;
->>>>>>> 912b4e869f1da62eafa073cdd8ee7435d8dba89d
 }
