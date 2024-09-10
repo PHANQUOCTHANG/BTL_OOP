c39fb4f117a6763c78662aae83c5a7b80113ec7f
QuocThang <phanquocthang211005@gmail.com>
2024-09-10 09:31:03 +0700

    fixx

---
+++ BTL_OOP/Account/Header.h
@@ -0,0 +1,43 @@
+#pragma once //Giúp gọi hàm lần 
+#include<bits/stdc++.h>
+#include <conio.h>
+using namespace std;
+
+
+class Account{
+    private:
+       string accountName,accountPassword; 
+    public:
+        string getAccountName();
+        string getAccountPassword();
+        void setAccountName(string &accountName);
+        void setAccountPassword(string &accountPassword);
+        void Register();
+        bool Login();
+        friend void userProcess();
+};
+class Admin:public Account{
+    private:
+        bool key;
+        
+    public:
+        Admin(){
+            this->key = 0;
+        }
+        void setKey(){
+            this->key = 1;
+        }
+        string getAdminName(){
+            return this->adminName;
+        }
+        string getadminPassword(){
+            return this->adminPassword;
+        }
+    protected:
+       const string adminName="admin",
+                    adminPassword="admin";
+
+};
+class User:public Account{
+    
+};
---
+++ BTL_OOP/Account/Interaction.cpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "Header.h"
+
+
+
+
+//Hàm set delay(độ chậm) cho giao diện
+void delay(int milliseconds) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+}
+//Hàm xử lý nhập mật khẩu
+std::string hidePassword(std::string &password) {
+    char ch;
+    while ((ch = _getch()) != '\r') { // '\r' là ký tự Enter
+        if (ch == '\b') { // Xóa ký tự nếu nhấn Backspace
+            if (!password.empty()) {
+                password.pop_back();
+                std::cout << "\b \b"; // Xóa dấu '*'
+            }
+        } else {
+            password.push_back(ch);
+            std::cout << '*'; // Hiển thị dấu '*'
+        }
+    }
+    std::cout << std::endl;
+    return password;
+}
+void showMenu1(){
+    cout<<"+-----------------"<<"\033[35m"<<"MENU"<<"\033[0m"<<"-----------------+\n";
+  cout << "|    1. Register          2. Login     |\n";
+    cout<<"+--------------------------------------+\n";
+    cout<<"Enter your choice: ";
+}
+void showMenuLogin(){
+    cout<<"+-----------------"<<"\033[35m"<<"LOGIN"<<"\033[0m"<<"-----------------+\n";
+  cout << "|    1. User               2. Admin     |\n";
+  cout << "|          3. Back to Menu              |\n";
+    cout<<"+---------------------------------------+\n";
+    cout<<"Enter your choice: ";
+}
